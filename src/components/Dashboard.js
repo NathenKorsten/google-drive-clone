1078f139c598399c4aea7558b4dead6927474f12
NathenKorsten <nktheastranout@gmail.com>
2021-02-15 08:39:34 +0200

    Finished Authentication

---
+++ src/components/Dashboard.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { Card, Button, Alert } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+
+export default function Dashboard() {
+  const { currentUser, logout } = useAuth();
+  const [error, setError] = useState("");
+  const history = useHistory();
+  const handleLogout = async () => {
+    setError("");
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  };
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Hi, {currentUser?.email}</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Link className="btn btn-primary w-100 mt-3" to="/update-profile">
+            Update profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  );
+}
